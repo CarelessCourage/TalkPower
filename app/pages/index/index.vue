a9dfe8c77be3f8c25435290e224070cef94a618a
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2026-04-11 05:31:55 +0200

    Update index.vue

--- app/pages/index/index.vue
+++ app/pages/index/index.vue
@@ -10,10 +10,34 @@ const enter = () => {
   <div class="SplashPage" @click="enter">
     <h1 class="SplashTitle">aura</h1>
     <p class="SplashTagline">read the room</p>
+    <!-- <div class="SplashRing">
+      <div class="SplashRingCap"></div>
+    </div> -->
   </div>
 </template>
 
 <style scoped>
+.SplashRing {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  background-color: aqua;
+  border-radius: 50%;
+  --size: 700px;
+  width: var(--size);
+  height: var(--size);
+  clip-path: polygon(100% 0%, 100% 50%, 0% 50%, 0% 0%);
+}
+
+.SplashRingCap {
+  --size: 90%;
+  width: var(--size);
+  height: var(--size);
+  background-color: var(--base);
+  border-radius: 100%;
+}
+
 .SplashPage {
   display: flex;
   flex-direction: column;
@@ -26,6 +50,8 @@ const enter = () => {
 }
 
 .SplashTitle {
+  position: relative;
+  z-index: 10;
   font-family: 'Montserrat Alternates', sans-serif;
   font-size: clamp(3rem, 10vw, 12rem);
   font-weight: 900;
@@ -37,6 +63,8 @@ const enter = () => {
 }
 
 .SplashTagline {
+  position: relative;
+  z-index: 10;
   font-size: clamp(1rem, 2vw, 1.25rem);
   color: var(--base-100);
   font-style: italic;
